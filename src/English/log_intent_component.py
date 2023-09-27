10702a22542f239fa6409ff8c7183911df5e695e
inuka-00 <77762018+inuka-00@users.noreply.github.com>
2023-09-27 17:55:35 +0530

    New component For Intent Extraction

---
+++ src/English/log_intent_component.py
@@ -0,0 +1,50 @@
+from typing import Dict, Text, Any, List
+
+from rasa.engine.graph import GraphComponent, ExecutionContext
+from rasa.engine.recipes.default_recipe import DefaultV1Recipe
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from rasa.shared.nlu.training_data.message import Message
+from rasa.shared.nlu.training_data.training_data import TrainingData
+
+# TODO: Correctly register your component with its type
+@DefaultV1Recipe.register(
+    [DefaultV1Recipe.ComponentType.INTENT_CLASSIFIER], is_trainable=True
+)
+class LogIntentsComponent(GraphComponent):
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> GraphComponent:
+        # TODO: Implement this
+        ...
+
+    def train(self, training_data: TrainingData) -> Resource:
+        # TODO: Implement this if your component requires training
+        ...
+
+    def process_training_data(self, training_data: TrainingData) -> TrainingData:
+        # TODO: Implement this if your component augments the training data with
+        #       tokens or message features which are used by other components
+        #       during training.
+        ...
+
+        return training_data
+
+    def process(self, messages: List[Message]) -> List[Message]:
+        # TODO: This is the method which Rasa Open Source will call during inference.
+        ...
+        print("Process Test")
+        print(messages[0].get_full_intent())
+        # intent = messages[0].get_full_intent()
+
+        # if intent['name']=='faq':
+        #     intent['name'] = messages[0].as_dict()['response_selector']['faq']['response']['intent_response_key']
+        #     intent['confidence'] = messages[0].as_dict()['response_selector']['faq']['response']['confidence']
+
+        # print(intent)
+        return messages
